870c4123b796d9380adbbaad4d7257cd517df3f8
Alan Sanchez <sanchezpineda03@gmail.com>
2017-03-29 21:44:58 -0600

    #4. Created js logic for infograph animation.

--- src/js/infograph.js
+++ src/js/infograph.js
@@ -1,3 +1,22 @@
 (function () {
-  
+  function animateItems() {
+    var windowHeight = $(window).outerHeight(),
+      scroll = $(window).scrollTop(),
+      items = $('.how-it-works__infograph-item').toArray();
+    items.forEach(function (elem, index) {
+      var _elem = $(elem),
+        top = _elem.offset().top,
+        height = _elem.height(),
+        trigger = scroll + windowHeight - height/2;
+      if (trigger >= top) {
+       _elem.addClass('show');
+      } else {
+       _elem.removeClass('show');
+      }
+    });
+  }
+  if ($('.how-it-works__infograph').length) {
+    animateItems();
+    $(window).scroll(animateItems);
+  }
 })();
